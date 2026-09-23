8a8d47133c44b2132922d6a665a613214dcc5d94
hocha333-ops <hocha333@gmail.com>
2026-09-23 20:22:08 +0300

    Add api/health.js

---
+++ api/health.js
@@ -0,0 +1,4 @@
+module.exports = function handler(req,res){
+  res.setHeader("Cache-Control","no-store");
+  res.status(200).json({ok:true,service:"makhachkala-live-bot",version:"2.5.0",telegramTokenConfigured:Boolean(process.env.TELEGRAM_BOT_TOKEN),channelConfigured:Boolean(process.env.TELEGRAM_CHANNEL),publishSecretConfigured:Boolean(process.env.PUBLISH_SECRET),autoPublish:process.env.AUTO_PUBLISH === "true"});
+};
