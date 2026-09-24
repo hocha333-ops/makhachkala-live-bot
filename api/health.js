2b82228e8919f1dd771f0e778f8260b6ec37255d
hocha333-ops <hocha333@gmail.com>
2026-09-24 19:30:23 +0300

    Align api/health.js with v2.11.0

--- api/health.js
+++ api/health.js
@@ -3,7 +3,7 @@ module.exports = function handler(req,res){
   res.status(200).json({
     ok:true,
     service:"makhachkala-live-bot",
-    version:"2.10.2",
+    version:"2.11.0",
     telegramTokenConfigured:Boolean(process.env.TELEGRAM_BOT_TOKEN),
     channelConfigured:Boolean(process.env.TELEGRAM_CHANNEL),
     publishSecretConfigured:Boolean(process.env.PUBLISH_SECRET),
